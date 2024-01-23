ce2015c077249f33a7d7dd7c9128628ef5ee512b
suzzie-nduta <112473115+suzzie-nduta@users.noreply.github.com>
2024-01-23 14:46:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-var today= new Date();
-var day= today.getDay();
-var daylist=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-console.log("Today is:"+ daylist[day]+".");
-var hour= today.getHours();
-var minutes= today.getMinutes();
-var seconds= today.getSeconds();
-var prepand=(hour>= 12)? " PM": "AM";
-if ( hour===0 && prepand=== 'PM'){
-if (minute ===0 && second ===0){
-    hour=12;
-    prepand='Noon';
-} else
-{
-    hour=12;
-    prepand='PM';
-}
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+console.log("Today is: " + daylist[day] + ".");
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+var prepand = (hour >= 12) ? " PM " : " AM ";
+hour = (hour >= 12) ? hour - 12 : hour;
+if (hour === 0 && prepand === ' PM ') {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        prepand = ' Noon';
+    } else {
+        hour = 12;
+        prepand = ' PM';
+    }
 }
 if (hour === 0 && prepand === ' AM ') {
     if (minute === 0 && second === 0) {
